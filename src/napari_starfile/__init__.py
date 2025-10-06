1de7cc806a51829bcd0ee2b863d05de4d503ae51
Moritz Wachsmuth-Melm <mail@moritzwm.de>
2025-10-06 15:30:42 +0200

    Add subset filtering widget

--- src/napari_starfile/__init__.py
+++ src/napari_starfile/__init__.py
@@ -6,13 +6,10 @@ except ImportError:
 
 from ._reader import napari_get_reader
 from ._sample_data import make_sample_data
-from ._widget import ExampleQWidget, ImageThreshold, threshold_autogenerate_widget, threshold_magic_widget
+from ._widget import SubsetSelectorWidget
 
 __all__ = (
     "napari_get_reader",
     "make_sample_data",
-    "ExampleQWidget",
-    "ImageThreshold",
-    "threshold_autogenerate_widget",
-    "threshold_magic_widget",
+    "SubsetSelectorWidget",
 )
