90549384a9a8926fb96d75c56bc8c5261500b1c4
Moritz Wachsmuth-Melm <mail@moritzwm.de>
2026-02-13 16:47:17 +0100

    Use scipy for rotation calcs

--- src/napari_starfile/_reader.py
+++ src/napari_starfile/_reader.py
@@ -1,14 +1,14 @@
 """
 https://napari.org/stable/plugins/building_a_plugin/guides.html#readers
 """
+import warnings
 from pathlib import Path
-from typing import Dict, Union, List, cast
+from typing import Dict, List, Union
 
 import numpy as np
 import pandas as pd
 import starfile
-
-from napari_starfile import utils
+from scipy.spatial.transform import Rotation
 
 
 def napari_get_reader(path: Union[str, List[str]]):
@@ -40,23 +40,23 @@ def read_star(star: Union[Dict[str, pd.DataFrame], pd.DataFrame]) -> List:
     coords = particles[[f"rlnCoordinate{zyx}" for zyx in "ZYX"]].to_numpy().astype(float)
     shift_columns = [f"rlnOrigin{zyx}Angst" for zyx in "ZYX"]
     if all(col in particles.columns for col in shift_columns):
-        shifts = particles[shift_columns].to_numpy().astype(float)
-        apix = particles["rlnPixelSize"].to_numpy().astype(float)
-        coords -= shifts/apix[:, None]
-    angles = np.deg2rad(particles[["rlnAngleRot", "rlnAngleTilt", "rlnAnglePsi"]].to_numpy())
-    rot_matrices = np.linalg.inv(utils.euler2matrix(angles, False))
+        warnings.warn("rlnOriginX/Y/ZAngst is not supported yet, ignoring")
+        # shifts = particles[shift_columns].to_numpy().astype(float)
+        # apix = particles["rlnPixelSize"].to_numpy().astype(float)
+        # coords -= shifts/apix[:, None]
+    rotations = Rotation.from_euler("ZYZ", particles[["rlnAngleRot", "rlnAngleTilt", "rlnAnglePsi"]].to_numpy(), degrees=True).inv()
     layers.append((coords, {"properties": particles}, "points"))
     # Axis vectors
     vecs_z = np.empty((len(coords), 2, 3), dtype=float)
     vecs_z[:, 0] = coords
-    vecs_z[:, 1] =  rot_matrices @ np.array([1, 0, 0])
+    vecs_z[:, 1] = rotations.apply([0, 0, 1])[:, ::-1]
     layers.append((vecs_z, {"name": "Z vectors", "edge_color": "blue"}, "vectors"))
     vecs_y = np.empty((len(coords), 2, 3), dtype=float)
     vecs_y[:, 0] = coords
-    vecs_y[:, 1] = rot_matrices @ np.array([0, 1, 0])
+    vecs_y[:, 1] = rotations.apply([0, 1, 0])[:, ::-1]
     layers.append((vecs_y, {"name": "Y vectors", "edge_color": "yellow"}, "vectors"))
     vecs_x = np.empty((len(coords), 2, 3), dtype=float)
     vecs_x[:, 0] = coords
-    vecs_x[:, 1] = rot_matrices @ np.array([0, 0, 1])
+    vecs_x[:, 1] = rotations.apply([1, 0, 0])[:, ::-1]
     layers.append((vecs_x, {"name": "X vectors", "edge_color": "red"}, "vectors"))
     return layers
