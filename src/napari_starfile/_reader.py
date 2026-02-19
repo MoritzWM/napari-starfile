ab7aeb3687f6c069c2a7abb6fb5a136d9d405457
Moritz Wachsmuth-Melm <mail@moritzwm.de>
2026-02-19 17:46:16 +0100

    Fix reader for relion 3

--- src/napari_starfile/_reader.py
+++ src/napari_starfile/_reader.py
@@ -12,59 +12,62 @@ def napari_get_reader(path: str | list[str]):
         path = [path]
     if not all(p.endswith(".star") for p in path):
         return None
-    return read_paths
+    return read_stars
 
 
-def read_paths(paths: str | list[str]) -> list:
+def read_stars(paths: str | list[str] | Path | list[Path]) -> list:
     paths = [paths] if isinstance(paths, str) else paths
+    paths = [Path(p) for p in paths]
     layers = []
     for path in paths:
-        layers += read_star(starfile.read(Path(path)))  # type: ignore
-    return layers
-
-
-def read_star(star: dict[str, pd.DataFrame] | pd.DataFrame) -> list:
-    if isinstance(star, pd.DataFrame):
-        star = {"particles": star}
-    if "particles" in star and "optics" in star:
-        try:
-            particles = pd.merge(
-                star["particles"],
-                star["optics"],
-                how="left",
-                left_on="rlnOpticsGroup",
-                right_on="rlnOpticsGroup",
-                validate="many_to_one",
-            )
-        except pd.errors.MergeError as err:
-            raise ValueError(
-                "Error: could not merge particles and optics"
-            ) from err
-    elif "" in star:
-        particles = star[""]
-    else:
-        raise ValueError("No particles in star file")
-    coords = (
-        particles[[f"rlnCoordinate{zyx}" for zyx in "ZYX"]]
-        .to_numpy()
-        .astype(float)
-    )
-    shift_columns = [f"rlnOrigin{zyx}Angst" for zyx in "ZYX"]
-    if all(col in particles.columns for col in shift_columns):
-        warnings.warn(
-            "rlnOriginX/Y/ZAngst is not supported yet, ignoring", stacklevel=2
+        star = starfile.read(path, always_dict=True)
+        if "particles" in star:
+            particles = star["particles"]
+        elif "" in star:
+            particles = star[""]
+        else:
+            raise ValueError("No particles in star file")
+        assert isinstance(particles, pd.DataFrame)
+        if "optics" in star:
+            try:
+                particles = pd.merge(
+                    particles,
+                    star["optics"],
+                    how="left",
+                    left_on="rlnOpticsGroup",
+                    right_on="rlnOpticsGroup",
+                    validate="many_to_one",
+                )
+            except pd.errors.MergeError as err:
+                raise ValueError(
+                    "Error: could not merge particles and optics"
+                ) from err
+        coords = (
+            particles[[f"rlnCoordinate{zyx}" for zyx in "ZYX"]]
+            .to_numpy()
+            .astype(float)
         )
-        # shifts = particles[shift_columns].to_numpy().astype(float)
-        # apix = particles["rlnPixelSize"].to_numpy().astype(float)
-        # coords -= shifts/apix[:, None]
-        #
-    rotations = Rotation.from_euler(
-        "ZYZ",
-        particles[["rlnAngleRot", "rlnAngleTilt", "rlnAnglePsi"]].to_numpy(),
-        degrees=True,
-    ).inv()
-    vecs_z = np.empty((len(coords), 2, 3), dtype=float)
-    vecs_z[:, 0] = coords
-    vecs_z[:, 1] = rotations.apply([0, 0, 1])[:, ::-1]
-    return [(vecs_z, {"name": "Z vectors", "edge_color": "blue"}, "vectors")]
-    # return [(coords, {"properties": particles}, "points")]
+        shift_columns = [f"rlnOrigin{zyx}Angst" for zyx in "ZYX"]
+        if all(col in particles.columns for col in shift_columns):
+            warnings.warn(
+                "rlnOriginX/Y/ZAngst is not supported yet, ignoring",
+                stacklevel=2,
+            )
+            # shifts = particles[shift_columns].to_numpy().astype(float)
+            # apix = particles["rlnPixelSize"].to_numpy().astype(float)
+            # coords -= shifts/apix[:, None]
+            #
+        rotations = Rotation.from_euler(
+            "ZYZ",
+            particles[
+                ["rlnAngleRot", "rlnAngleTilt", "rlnAnglePsi"]
+            ].to_numpy(),
+            degrees=True,
+        ).inv()
+        vecs = np.empty((len(coords), 2, 3), dtype=float)
+        vecs[:, 0] = coords
+        vecs[:, 1] = rotations.apply([0, 0, 1])[:, ::-1]
+        layers.append(
+            (vecs, {"name": path.stem, "edge_color": "blue"}, "vectors")
+        )
+    return layers
--- src/napari_starfile/_sample_data.py
+++ src/napari_starfile/_sample_data.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
-from pathlib import Path
-import starfile
 
-from napari_starfile._reader import read_star
+from pathlib import Path
+
+from napari_starfile._reader import read_stars
 
 
 def make_sample_data():
-    return read_star(starfile.read(Path(__file__).parent / "data" / "example_particles.star"))
+    return read_stars(
+        Path(__file__).parent / "data" / "example_particles.star"
+    )
