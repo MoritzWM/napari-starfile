4120f34ef84e6fc019c00ba90055d586d737c150
Moritz Wachsmuth-Melm <mail@moritzwm.de>
2026-02-19 17:46:48 +0100

    Add writer stubs

--- src/napari_starfile/_writer.py
+++ src/napari_starfile/_writer.py
@@ -6,9 +6,10 @@ see: https://napari.org/stable/plugins/building_a_plugin/guides.html#writers
 
 Replace code below according to your needs.
 """
+
 from collections.abc import Sequence
 from pathlib import Path
-from typing import TYPE_CHECKING, Any, Union, List
+from typing import TYPE_CHECKING, Any
 
 import numpy as np
 import pandas as pd
@@ -17,12 +18,12 @@ import starfile
 from napari_starfile.utils import vec2euler
 
 if TYPE_CHECKING:
-    DataType = Union[Any, Sequence[Any]]
+    DataType = Any | Sequence[Any]
     FullLayerData = tuple[DataType, dict, str]
 
 
 def _layers_to_df(
-        data: List["FullLayerData"], layer_column_name: str
+    data: list["FullLayerData"], layer_column_name: str
 ) -> pd.DataFrame:
     """Turn a list of FullLayerData of vectors into a DataFrame with particle
     coordinates.
@@ -42,11 +43,44 @@ def _layers_to_df(
             }
         )
         dfs.append(df)
+        coords = (
+            particles[[f"rlnCoordinate{zyx}" for zyx in "ZYX"]]
+            .to_numpy()
+            .astype(float)
+        )
+        shift_columns = [f"rlnOrigin{zyx}Angst" for zyx in "ZYX"]
+        if all(col in particles.columns for col in shift_columns):
+            warnings.warn("rlnOriginX/Y/ZAngst is not supported yet, ignoring")
+            # shifts = particles[shift_columns].to_numpy().astype(float)
+            # apix = particles["rlnPixelSize"].to_numpy().astype(float)
+            # coords -= shifts/apix[:, None]
+        rotations = Rotation.from_euler(
+            "ZYZ",
+            particles[
+                ["rlnAngleRot", "rlnAngleTilt", "rlnAnglePsi"]
+            ].to_numpy(),
+            degrees=True,
+        ).inv()
     return pd.concat(dfs)
 
-def write_star_relion3(path: str, data: List["FullLayerData"]) -> List[str]:
+
+def write_star_relion3(path: str, data: list["FullLayerData"]) -> list[str]:
     if not path.endswith(".star"):
         path += ".star"
-    particles = _layers_to_df(data, "rlnMicrographName")
+    for layer_data, layer_meta, layer_type in data:
+        if layer_type != "vectors":
+            raise ValueError(f"Unsupported layer type: {layer_type}")
     starfile.write(particles, Path(path), overwrite=True)
     return [path]
+
+
+def write_star_relion31(path: str, data: list["FullLayerData"]) -> list[str]:
+    if not path.endswith(".star"):
+        path += ".star"
+    raise NotImplementedError("write_star_relion31 is not implemented yet")
+
+
+def write_star_relion5(path: str, data: list["FullLayerData"]) -> list[str]:
+    if not path.endswith(".star"):
+        path += ".star"
+    raise NotImplementedError("write_star_relion5 is not implemented yet")
