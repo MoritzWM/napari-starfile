612090ee1ef191e5700bc4c5256af234ea960b24
MoritzWM <github@moritzwm.de>
2026-02-24 17:28:02 +0100

    Implement relion3.1 writer

--- src/napari_starfile/_writer.py
+++ src/napari_starfile/_writer.py
@@ -10,6 +10,7 @@ Replace code below according to your needs.
 from collections.abc import Sequence
 from pathlib import Path
 from typing import TYPE_CHECKING, Any
+import warnings
 
 import numpy as np
 import pandas as pd
@@ -63,22 +64,62 @@ def _layers_to_df(
         ).inv()
     return pd.concat(dfs)
 
+def _verify_table(df: pd.DataFrame):
+        required_columns = [f"rlnCoordinate{zyx}" for zyx in "ZYX"] + [f"rlnAngle{angle}" for angle in ["Rot", "Tilt", "Psi"]]
+        missing_columns = [col for col in required_columns if col not in df.columns]
+        if missing_columns:
+            raise ValueError(f"Missing required columns: {', '.join(missing_columns)}")
 
 def write_star_relion3(path: str, data: list["FullLayerData"]) -> list[str]:
     if not path.endswith(".star"):
         path += ".star"
+    all_particles: list[pd.DataFrame] = []
     for layer_data, layer_meta, layer_type in data:
         if layer_type != "vectors":
             raise ValueError(f"Unsupported layer type: {layer_type}")
-    starfile.write(particles, Path(path), overwrite=True)
+        particles = layer_meta["features"]
+        _verify_table(particles)
+        particles["rlnMicrographName"] = layer_meta["name"].replace(" ", "_")
+        if "optics" in layer_meta["metadata"]:
+            try:
+                particles = pd.merge(
+                    particles,
+                    layer_meta["metadata"]["optics"],
+                    how="left",
+                    left_on="rlnOpticsGroup",
+                    right_on="rlnOpticsGroup",
+                    validate="many_to_one",
+                )
+            except pd.errors.MergeError as err:
+                raise ValueError(
+                    "Error: could not merge particles and optics"
+                ) from err
+        all_particles.append(particles)
+    particles = pd.concat(all_particles, ignore_index=True, join="inner")
+    starfile.write(all_particles, Path(path), overwrite=True)
     return [path]
 
 
 def write_star_relion31(path: str, data: list["FullLayerData"]) -> list[str]:
     if not path.endswith(".star"):
         path += ".star"
-    raise NotImplementedError("write_star_relion31 is not implemented yet")
-
+    all_particles: list[pd.DataFrame] = []
+    all_optics: list[pd.DataFrame] | pd.DataFrame = []
+    for layer_data, layer_meta, layer_type in data:
+        if layer_type != "vectors":
+            raise ValueError(f"Unsupported layer type: {layer_type}")
+        particles = layer_meta["features"]
+        _verify_table(particles)
+        particles["rlnMicrographName"] = layer_meta["name"].replace(" ", "_")
+        if "optics" in layer_meta["metadata"]:
+            all_optics.append(layer_meta["metadata"]["optics"])
+        all_particles.append(particles)
+    particles = pd.concat(all_particles, ignore_index=True, join="inner")
+    if len(all_optics) > 1:
+        warnings.warn("Joining optics tables from different layers. Make sure they are all the same!", stacklevel=2)
+    optics = pd.concat(all_optics, join="inner").drop_duplicates().reset_index(drop=True)
+    starfile.write({"optics": optics, "particles": particles}, Path(path), overwrite=True)
+    return [path]
 
 def write_star_relion5(path: str, data: list["FullLayerData"]) -> list[str]:
     if not path.endswith(".star"):
